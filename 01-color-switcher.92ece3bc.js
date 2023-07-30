54cfa4ffbab8a535b67a992312e96488ffeb405e
SaltyUA <104589666+SaltyUA@users.noreply.github.com>
2023-07-30 10:00:11 +0000

    Deploying to gh-pages from @ SaltyUA/goit-js-hw-09@5676a7b09cdbf3c47b0db57556ad2d96f5e89808 🚀

---
+++ 01-color-switcher.92ece3bc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.92ece3bc.js.map
